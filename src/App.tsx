a71a6af08da797bab403f947fc6c686180ae0710
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-01-02 21:18:58 +0530

    Fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import OnlineTicTacToe from "./components/Online_TicTac_Toe/OnlineTicTacToe.tsx";
 import TicTacToe from "./components/Tic_Tac_Toe/TicTacToe.tsx";
 
 function App() {
   return (
     <>
-      {/* <TicTacToe /> */}
-      <OnlineTicTacToe />
+      <TicTacToe />
     </>
   );
 }
